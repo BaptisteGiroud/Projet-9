886bae501bdc6cf96cc85fea1152eaf115245948
Baptiste Giroud <giroudbpro@gmail.com>
2024-03-05 13:30:06 +0100

    Ajout meta schema.org - PurgeCSS

---
+++ purgecss.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "index.html",
+    "./assets/scripts.js",
+    "./assets/maugallery.js",
+    "./assets/mediaCarousel.js",
+    "./assets/bootstrap/bootstrap.bundle.js",
+    "./assets/bootstrap/bootstrap.bundle.js.map",
+    "./assets/bootstrap/bootstrap.bundle.min.js",
+    "./assets/bootstrap/bootstrap.bundle.min.js.map",
+  ],
+  css: ["./assets/bootstrap/bootstrap.css"],
+  keyframes: true,
+  output: "./assets/purified/",
+  rejected: true,
+  variables: true,
+};
